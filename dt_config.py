b75ff861746b6a7ee5befdcb63eb1c09393cd1a2
pydeoxy <149623594+pydeoxy@users.noreply.github.com>
2024-10-24 16:23:19 +0300

    Update mqtt_topics

--- dt_config.py
+++ dt_config.py
@@ -2,6 +2,9 @@ CONFIG = {
     #'ifc_file': 'path/to/building.ifc',
     'mqtt_broker': 'xrdevmqtt.edu.metropolia.fi',
     'mqtt_port': 1883,
-    'mqtt_topic': 'M-bus/Electricity/Current L2',
+    'mqtt_topics': [# 'M-bus/Electricity/Current L2'
+                    'KNX/15/0/3<Bathroom.Sensors.Floor-temp-C>', 
+                    'KNX/15/0/0<Bathroom.Sensors.CO2-ppm>' 
+                    ],
     'db_path': 'C:/Users/yanpe/OneDrive - Metropolia Ammattikorkeakoulu Oy/Courses/DTIC/cic_dt_smartlab/sensor_data.db'
 }
