b3db606fb3f4de7ab135a10fd9573d9e57b99198
pydeoxy <149623594+pydeoxy@users.noreply.github.com>
2024-10-24 21:37:17 +0300

    Update dt_config.py

--- dt_config.py
+++ dt_config.py
@@ -3,8 +3,17 @@ CONFIG = {
     'mqtt_broker': 'xrdevmqtt.edu.metropolia.fi',
     'mqtt_port': 1883,
     'mqtt_topics': [# 'M-bus/Electricity/Current L2'
+                    'KNX/13/0/2<Livingroom.Sensors.Air-temperature-C>',
+                    'KNX/13/0/3<Livingroom.Sensors.Floor-temp-C>',
+                    'KNX/13/0/0<Livingroom.Sensors.CO2-ppm>',
+                    'KNX/13/0/1<Livingroom.Sensors.Rh-percent>',
+                    'KNX/14/0/2<Bedroom.Sensors.Air-temperature-C>',
+                    'KNX/14/0/0<Bedroom.Sensors.CO2-ppm>',
+                    'KNX/14/0/1<Bedroom.Sensors.Rh-percent>',
+                    'KNX/15/0/2<Bathroom.Sensors.Air-temperature-C>',
                     'KNX/15/0/3<Bathroom.Sensors.Floor-temp-C>', 
-                    'KNX/15/0/0<Bathroom.Sensors.CO2-ppm>' 
+                    'KNX/15/0/0<Bathroom.Sensors.CO2-ppm>',
+                    'KNX/15/0/1<Bathroom.Sensors.RH|percent>'
                     ],
     'db_path': 'C:/Users/yanpe/OneDrive - Metropolia Ammattikorkeakoulu Oy/Courses/DTIC/cic_dt_smartlab/sensor_data.db'
 }
